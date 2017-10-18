b18ece4b347c0c9eef3782e4893bca36041645be
aaronRhodebeck <arhodebeck@gmail.com>
2017-10-18 11:53:42 -0500

    Merge finished grayscale layout

--- app/index.js
+++ app/index.js
@@ -6,8 +6,41 @@ import { render } from 'react-dom';
 // Redux imports
 import { Provider } from 'react-redux';
 import { store } from './store';
+
+// Calculator imports
+import {
+  Calculator,
+  CalcButton,
+  ButtonArea,
+  Display,
+  DisplayResult,
+  DisplayFormula,
+} from './layout/DisplayComponents';
 // #endregion
 
-const app = <Provider store={store} />;
+const app = (
+  <Provider store={store}>
+    <Calculator>
+      <Display>
+        <DisplayFormula>10 + 20 - 4 * 27 / 5</DisplayFormula>
+        <DisplayResult>10098</DisplayResult>
+      </Display>
+      <ButtonArea>
+        <CalcButton />
+        <CalcButton />
+        <CalcButton />
+        <CalcButton />
+        <CalcButton />
+        <CalcButton />
+        <CalcButton />
+        <CalcButton />
+        <CalcButton />
+        <CalcButton />
+        <CalcButton />
+        <CalcButton />
+      </ButtonArea>
+    </Calculator>
+  </Provider>
+);
 
 render(app, document.getElementById('app'));
---
+++ app/layout/DisplayComponents.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+
+// Outer structure of the calculator
+export const Calculator = styled.div`
+  margin: auto;
+  margin-top: 5vh;
+  border-style: solid;
+  border-radius: 20px;
+  border-width: 15px;
+  height: 80vh;
+  width: 50vh;
+  position: relative;
+  * {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+`;
+
+export const ButtonArea = styled.div`
+  border-top: 2px solid black;
+  height: 73%;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  position: absolute;
+  bottom: 0;
+`;
+
+export const CalcButton = styled.button`
+  margin: 3px;
+  min-width: 23%;
+  border: 2px outset gray;
+  outline: none;
+  background: #888;
+
+  &:active {
+    border: 2px inset gray;
+  }
+`;
+
+export const Display = styled.div`
+  height: 25%;
+  border: 3px inset darkgray;
+  margin: 5px;
+  background: radial-gradient(at top left, #ddd, #eee);
+  position: relative;
+`;
+
+export const DisplayResult = styled.p`
+  color: black;
+  font-weight: bold;
+  font-size: 10vmin;
+  position: absolute;
+  bottom: 5%;
+  right: 10px;
+  margin: 0;
+`;
+
+export const DisplayFormula = styled.p`
+  color: black;
+  font-size: 4vmin;
+  position: absolute;
+  top: 5%;
+  right: 10px;
+  margin: 0;
+`;
