51b022536a05c252a99e56b0c7e64a903a45a8d5
aaronRhodebeck <arhodebeck@gmail.com>
2017-10-20 22:31:57 -0500

    Remove unecessary imports and add dev tools

--- app/store.js
+++ app/store.js
@@ -1,7 +1,11 @@
 // #region imports
 import { createStore } from 'redux';
 import rootReducer from './reducers/rootReducer';
+
 // #endregion
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 export default store;
