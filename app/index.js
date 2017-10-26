9d37f5749cb94be64bfb02feba33fad8353b4160
aaronRhodebeck <arhodebeck@gmail.com>
2017-10-26 15:06:32 -0500

    Fix merge conflicts

--- app/index.js
+++ app/index.js
@@ -5,7 +5,6 @@ import { render } from 'react-dom';
 
 // Redux imports
 import { Provider } from 'react-redux';
-<<<<<<< HEAD
 import store from './store';
 
 // Calculator imports
@@ -14,47 +13,6 @@ import App from './containers/App';
 const app = (
   <Provider store={store}>
     <App />
-=======
-import { store } from './store';
-
-// Calculator imports
-import {
-  Calculator,
-  CalcButton,
-  ButtonArea,
-  Display,
-  DisplayResult,
-  DisplayFormula,
-} from './layout/DisplayComponents';
-// #endregion
-
-const app = (
-  <Provider store={store}>
-<<<<<<< HEAD
-    <Calculator />
->>>>>>> WIP Switch context
-=======
-    <Calculator>
-      <Display>
-        <DisplayFormula>10 + 20 - 4 * 27 / 5</DisplayFormula>
-        <DisplayResult>10098</DisplayResult>
-      </Display>
-      <ButtonArea>
-        <CalcButton />
-        <CalcButton />
-        <CalcButton />
-        <CalcButton />
-        <CalcButton />
-        <CalcButton />
-        <CalcButton />
-        <CalcButton />
-        <CalcButton />
-        <CalcButton />
-        <CalcButton />
-        <CalcButton />
-      </ButtonArea>
-    </Calculator>
->>>>>>> Create grayscale layout of calculator
   </Provider>
 );
 
