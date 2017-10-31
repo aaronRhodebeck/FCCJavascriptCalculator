5061ef74db2378d2e0c32ccb36ad519e76306d70
aaronRhodebeck <arhodebeck@gmail.com>
2017-10-31 12:29:58 -0500

    Setup githup pages

--- app/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-// #region imports
-import { combineReducers } from 'redux';
-// import all reducers from app
-import calculate from './runCalculator';
-// #endregion
-
-const rootReducer = combineReducers({ calculate });
-export default rootReducer;
--- spec/support/jasmineSetupTest.spec.js
+++
@@ -1,8 +0,0 @@
-// describe("Jasmine test", () => {
-//   it("should have passing test", () => expect(true).toBe(true));
-//   it("should have a failing test", () => expect(true).toBe(false));
-//   it("should have a pending test", () => {
-//     pending("This test is marked as pending");
-//     expect(true).toBe(true);
-//   });
-// });
